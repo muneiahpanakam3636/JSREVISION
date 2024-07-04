e39844a186ea9b322563a059a0ad13a08adbf477
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-04 18:40:19 +0530

    tst

---
+++ even.js
@@ -0,0 +1,14 @@
+// let num = 337;
+// if (num%2===0){
+//     console.log("even number");
+// }else{
+//     console.log("odd number");
+// }
+
+if("salman"){
+    console.log("still bachelor");
+}
+else{
+    console.log("don't know");
+}
+
