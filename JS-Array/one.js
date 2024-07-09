f34aa052b9c2e83ad16d48a7ac6b54ede7b67e18
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-09 18:06:35 +0530

    tst

---
+++ JS-Array/one.js
@@ -0,0 +1,10 @@
+let eids=[101,102,103,104,105,106]
+
+// for(i=0;i<=5;i++){
+//     console.log(eids[i]);
+// }
+console.log(eids[0]);
+console.log(eids[1]);
+console.log(eids[2]);
+console.log(eids[3]);
+console.log(eids[4]);
---
+++ JS-Array/two.js
@@ -0,0 +1,15 @@
+let eids=[101,102,103,104,105,106]
+
+// for(i=0;i<=5;i++){
+//     console.log(eids[i]);
+// }
+// let i=0;
+// while(i<=5){
+//     console.log(eids[i]);
+//     i++
+// }
+let i =0;
+do{
+    console.log(eids[i]);
+    i++;
+}while(i<=5)
