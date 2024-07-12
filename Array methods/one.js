e9dc3c4cf5ba07f9ea98bea651e2287e727c5c52
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-12 18:23:18 +0530

    tst

---
+++ Array methods/one.js
@@ -0,0 +1,7 @@
+let eids = [101,102,103,104]
+
+//adding new element//
+let new_len=eids.push(105)
+console.log(eids);
+console.log(new_len);
+
---
+++ Array methods/two.js
@@ -0,0 +1,5 @@
+let eids = [101,102,103,104]
+//delete last element from array//
+
+eids.pop(105);
+console.log(eids);
