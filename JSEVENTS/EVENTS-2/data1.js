994f56609e2011344ce8cbeea1b4359267e09f84
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-15 18:51:26 +0530

    tst

---
+++ JSEVENTS/EVENTS-2/data1.js
@@ -0,0 +1,10 @@
+function Change_colour1(){
+    document.getElementById('abc').innerHTML="Test case"
+}
+
+function Change_colour2(){
+    document.getElementById('xyz').innerHTML="Login"
+}
+function Change_colour3(){
+    document.getElementById("123").innerHTML="Contact"
+}
