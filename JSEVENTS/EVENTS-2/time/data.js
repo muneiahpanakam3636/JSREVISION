a27455adf7aa6da411a88d06d89047320e3095fc
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-16 12:50:38 +0530

    events

---
+++ JSEVENTS/EVENTS-2/time/data.js
@@ -0,0 +1,13 @@
+let pTag_Ref =document.getElementById('muni')
+console.log(pTag_Ref)
+pTag_Ref.innerHTML = new Date().toLocaleTimeString()
+
+
+
+
+
+
+// let pTag_Ref=document.getElementsByTagName('p')[0]
+// console.log(pTag_Ref)
+
+// pTag_Ref.innerHTML=new Date().toLocaleTimeString()
