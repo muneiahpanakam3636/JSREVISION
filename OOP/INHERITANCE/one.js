8d8334cb9b6240f7e2215cfbc378e86aa8d8aac8
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-30 18:03:55 +0530

    tst

---
+++ OOP/INHERITANCE/one.js
@@ -0,0 +1,12 @@
+
+//without inheritance
+
+class parent {
+    assets = '10 CR';
+}
+class Child  {
+    qualities = 'good'
+
+}
+let c1 = new Child();
+console.log(c1);
---
+++ OOP/INHERITANCE/two.js
@@ -0,0 +1,9 @@
+class parent {
+    assets = '10 CR';
+}
+class Child extends parent {
+    qualities = 'good'
+
+}
+let c1 = new Child();
+console.log(c1);
