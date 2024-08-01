9e0a7d2fd0d4131ce11b2e37c0111209ab19d0ff
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-08-01 11:35:00 +0530

    tst

---
+++ ES6-ASYNC/syncvsasync/two.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    console.log('Request-1');
+},1000)
+setTimeout(()=>{
+    console.log('Request-2');
+},3000)
+setTimeout(()=>{
+    console.log('Request-3');
+},6000)
+setTimeout(()=>{
+    console.log('Request-4');
+},2000)
+setTimeout(()=>{
+    console.log('Request-5');
+},4000)
